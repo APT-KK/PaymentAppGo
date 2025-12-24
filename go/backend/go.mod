3ec31c7606ca6fabeb42469d37e98fd7f819873d
APT-KK <khoslakabeer527@gmail.com>
2025-12-24 22:55:37 +0530

    add go library for stripe

---
+++ go/backend/go.mod
@@ -0,0 +1,5 @@
+module my-payment-app
+
+go 1.25.5
+
+require github.com/stripe/stripe-go/v81 v81.4.0 // indirect
--- go/backend/server.go
+++ go/backend/server.go
@@ -1,13 +1,16 @@
 package main
 
+// to export something it should start with a capital letter
 import (
-	"fmt"
+	"encoding/json"
+	// "fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
+
 	http.HandleFunc("/", handleHealth)
 	var err error = http.ListenAndServe("localhost:8080", nil)
 	if(err != nil){
@@ -16,7 +19,34 @@ func main() {
 }
 
 func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
- fmt.Println("hiiii")
+
+   if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+   }
+
+   var req struct {
+		ProductId string `json:"product_id"`
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+		Address1  string `json:"address_1"`
+		Address2  string `json:"address_2"`
+		City      string `json:"city"`
+		State     string `json:"state"`
+		Zip       string `json:"zip"`
+		Country   string `json:"country"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&req) // store request body in req variable
+	if(err != nil){
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// params := &stripe.PaymentIntentParams{
+		
+	// }
+
 }
 
 func handleHealth(w http.ResponseWriter, r *http.Request) {
